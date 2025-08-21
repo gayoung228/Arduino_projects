b6fc9bbb41d508069ae0d6e8ea4ff59fd4f42c91
이가영 <103436798+gayoung228@users.noreply.github.com>
2025-08-21 23:27:21 +0900

    Update DHT11_1602.ino

--- DHT11_1602.ino
+++ DHT11_1602.ino
@@ -1,6 +1,6 @@
 #include <LiquidCrystal.h> //LiquidCrystal.h 라이브러리 포함
 #include "DHT.h" //DHT.h 라이브러리 포함
-DHT dht(8, DHT11); //DHT 설정(DHT11 센서: 데이터핀 = D8)
+DHT dht(8, DHT11); //DHT 설정(DHT11 센서: 데이터핀 = D8) 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2); // LCD 연결 핀 설정
 
 void setup() {
